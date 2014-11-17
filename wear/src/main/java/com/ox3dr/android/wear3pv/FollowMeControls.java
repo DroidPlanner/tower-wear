8cdbb0a455dc4760a8a533a00b53ec884799e454
ne0fhyk <fhuyakou@gmail.com>
2014-11-17 08:49:13 -0800

    added 3dr services client library dependency.

--- wear/src/main/java/com/ox3dr/android/wear3pv/FollowMeControls.java
+++ wear/src/main/java/com/ox3dr/android/wear3pv/FollowMeControls.java
@@ -1,13 +1,23 @@
 package com.ox3dr.android.wear3pv;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
-public class FollowMeControls extends Activity {
+import com.o3dr.android.client.DPApiCallback;
+import com.o3dr.android.client.Drone;
+import com.o3dr.android.client.ServiceListener;
+import com.o3dr.android.client.ServiceManager;
+
+public class FollowMeControls extends Activity implements ServiceListener {
 
     private TextView mTextView;
+
+    private Drone drone;
+    private ServiceManager serviceMgr;
+    private DPApiCallback apiCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +28,35 @@ public class FollowMeControls extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+
             }
         });
+
+        final Context context = getApplicationContext();
+        serviceMgr = new ServiceManager(context);
+        drone = new Drone(context, serviceMgr);
+        apiCallback = new DPApiCallback(context);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        serviceMgr.connect(this);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        serviceMgr.disconnect(this);
+    }
+
+    @Override
+    public void onServiceConnected() {
+
+    }
+
+    @Override
+    public void onServiceDisconnected() {
+
     }
 }

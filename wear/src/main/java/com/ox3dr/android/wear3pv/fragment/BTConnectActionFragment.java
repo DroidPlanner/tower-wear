bfa0f13c69ac1660c6eeb9848a356c7648981ce4
ne0fhyk <fhuyakou@gmail.com>
2014-11-19 15:43:58 -0800

    hardcoded the bluetooth address to connect to.

--- wear/src/main/java/com/ox3dr/android/wear3pv/fragment/BTConnectActionFragment.java
+++ wear/src/main/java/com/ox3dr/android/wear3pv/fragment/BTConnectActionFragment.java
@@ -8,6 +8,9 @@ import com.ox3dr.android.wear3pv.R;
  * Created by fhuya on 11/17/14.
  */
 public class BTConnectActionFragment extends BaseActionFragment {
+
+    private static final String BLUETOOTH_ADDRESS = "30:14:09:02:22:45";
+
     @Override
     protected int getActionImageResource() {
         return android.R.drawable.stat_sys_data_bluetooth;

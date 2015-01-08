19cfa90436912b170a210acbe96e826c7c7571fe
ne0fhyk <fhuyakou@gmail.com>
2015-01-07 16:02:11 -0800

    cleaned up app preferences.

--- mobile/src/main/java/com/o3dr/android/dp/wear/utils/AppPreferences.java
+++ mobile/src/main/java/com/o3dr/android/dp/wear/utils/AppPreferences.java
@@ -3,6 +3,7 @@ package com.o3dr.android.dp.wear.utils;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.text.TextUtils;
 
 import com.o3dr.android.dp.wear.R;
 import com.o3dr.services.android.lib.drone.connection.ConnectionType;
@@ -131,15 +132,10 @@ public class AppPreferences {
     }
 
     public void setDronesharePassword(String b) {
-        prefs.edit().putString(context.getString(R.string.pref_dshare_password_key), b.trim())
-                .apply();
+        prefs.edit().putString(context.getString(R.string.pref_dshare_password_key), b.trim()).apply();
     }
 
     public boolean getDroneshareEnabled() {
-        return prefs.getBoolean(context.getString(R.string.pref_dshare_enabled_key), true);
-    }
-
-    public void setDroneshareEnabled(boolean b) {
-        prefs.edit().putBoolean(context.getString(R.string.pref_dshare_enabled_key), b).apply();
+        return !TextUtils.isEmpty(getDroneshareLogin()) && !TextUtils.isEmpty(getDronesharePassword());
     }
 }
